0678a71d1552c29a47059d27cab3f38722cb0226
Bas Buvelot <s.f.buvelot@gmail.com>
2018-10-12 20:41:32 +0200

    added gitignore and fixed mapping

---
+++ DHDomtica/Models/MainCategoryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace DHDomtica.Models
+{
+    public class MainCategory
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
