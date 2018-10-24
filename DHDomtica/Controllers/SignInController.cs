f0c498087d6e2a06947caba3731113f1f475b5c2
DZoetmulder <dannyzoetmulder@gmail.com>
2018-10-24 18:09:58 +0200

    Een sign in en signup controller toegevoegd en een extra vakje voor de stad van de user
    Ben dicht bij het fixen van sign up. Moet alleen nog html en controllers aan elkaar linken.

---
+++ DHDomtica/Controllers/SignInController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using DHDomtica.Models;
+
+namespace MvcLoginAppDemo.Controllers
+{
+    public class SignInController : Controller
+    {
+        public ActionResult InlogPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InlogPage(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DHDomoticaDBEntities db = new DHDomoticaDBEntities())
+                {
+                    var obj = db.User.Where(a => a.EMail.Equals(userModel.EMail) && a.Password.Equals(userModel.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.ID.ToString();
+                        Session["UserEmail"] = obj.EMail.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(userModel);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("InlogPage");
+            }
+        }
+    }
+}
---
+++ DHDomtica/Controllers/SignUpController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DHDomtica.Models;
+
+// Deze en andere code voor sign-up zijn gebaseerd op de voorbeeldcode van CodAffection bereikbaar op https://www.youtube.com/watch?v=xBS9FMF2NFM
+
+namespace DHDomtica.Controllers
+{
+    public class SignUpController : Controller
+    {
+        [HttpGet]
+        // GET: SignUp
+        public ActionResult SignUpPage(int id = 0)
+        {
+            User usermodel = new User();
+            return View(usermodel);
+        }
+
+        // GET: SignUp/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SignUp/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SignUp/Create
+        [HttpPost]
+        public ActionResult SignUpPage(User userModel)
+        {
+            using (DHDomoticaDBEntities DHDomoticadbModel = new DHDomoticaDBEntities())
+            // If loop met passwordcheck gelijk aan gebruikersnaamcheck hangen.
+            {
+                if (DHDomoticadbModel.User.Any(x => x.EMail == userModel.EMail))
+                {
+                    ViewBag.DuplicateMessage = "E-mail is al in gebruik. Probeer een ander E-mailadres";
+                    return View("SignUpPage", userModel);
+                }
+
+                DHDomoticadbModel.User.Add(userModel);
+                DHDomoticadbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Uw account is geregistreerd";
+            return View("InlogPage", new User());
+        }
+
+        // GET: SignUp/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SignUp/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("SignUpPage");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SignUp/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SignUp/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("SignUpPage");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
