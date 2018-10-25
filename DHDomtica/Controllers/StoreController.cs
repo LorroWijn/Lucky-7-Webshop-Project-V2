1d2d0ba25ffec63dac612500d47c4e339059676e
Bas Buvelot <s.f.buvelot@gmail.com>
2018-10-25 14:46:58 +0200

    producten kunnen uit de DB gehaald worden.

---
+++ DHDomtica/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DHDomtica.Models;
+
+namespace DHDomtica.Controllers
+{
+    public class StoreController : Controller
+    {
+        private DHDomoticaDBEntities db = new DHDomoticaDBEntities();
+
+        // GET: Store
+        public ViewResult Index()
+        {
+            return View(db.Product.ToList());
+        }
+    }
+}
