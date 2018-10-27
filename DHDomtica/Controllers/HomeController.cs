cf57ac51ad50e2f569a9148e37a4d77423b6f4fa
Bas Buvelot <s.f.buvelot@gmail.com>
2018-10-27 21:20:08 +0200

    cleaned up home controller and created an storecontroller and some other shit just look

--- DHDomtica/Controllers/HomeController.cs
+++ DHDomtica/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Core.Common.CommandTrees;
 using System.Linq;
 using System.Data.Linq.Mapping;
+using System.Net;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
@@ -34,7 +35,6 @@ namespace DHDomtica.Controllers
 
         public ActionResult About()
         {
-            ShowSidebar();
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -42,12 +42,11 @@ namespace DHDomtica.Controllers
 
         public ActionResult Contact()
         {
-            ShowSidebar();
             ViewBag.Message = "Your contact page.";
             return View();
         }
 
-        public ActionResult ShoppingCart()
+       public ActionResult ShoppingCart()
         {
             ViewBag.Message = "Welcome in the shoppingcart";
 
@@ -59,63 +58,12 @@ namespace DHDomtica.Controllers
             return View();
         }
 
-        public ActionResult Categories(int id)
-        {
-            ShowSidebar();
-
-            var categorie = _context.MainCategory.SingleOrDefault(c => c.ID == id);
-
-            if (categorie == null)
-                return HttpNotFound();
-            var ProductList = new MainCategoryViewModel()
-            {
-                Category = categorie,
-                Products = _context.Product.Where(c => c.MainCategoryID.Equals(id)).ToList().AsEnumerable()
-
-            };
-            return View(ProductList);
-        }
-
-        public ActionResult Products(int id)
-        {
-            ShowSidebar();
-
-            var product = _context.Product.SingleOrDefault(p => p.ID == id);
-
-            if (product == null)
-                return HttpNotFound();
-
-            return View(product);
-        }
-
-
         private void ShowSidebar()
         {
             System.Diagnostics.Debug.WriteLine($"Sidebar {Request.RawUrl}");
             ViewBag.ShowSideBar = true;
             ViewBag.AllCategories = _context.MainCategory.ToList();
-            ViewBag.AllProducts = GetProducts();
-        }
-
-        private IEnumerable<Product> GetProducts()
-        {
-
-            using (var db = new MyDBContext())
-            {
-
-                /*var MainCategorySelected = "Smart Home";*/
-
-                /*var QuerydProducts = (from Product in db.Products
-                                      where Product.Maincategory == MainCategorySelected
-                                      select Product);*/
-
-                //IQueryable<object> q = QuerydProducts;
-                //List<object> l = new List<object>(q);
-/*
-
-                return QuerydProducts.ToList();*/
-                return new List<Product>();
-            }
+            ViewBag.AllProducts = _context.Product.ToList();
         }
 
         public ActionResult Search(string searchString)
@@ -129,19 +77,21 @@ namespace DHDomtica.Controllers
 
                 if (categorie == null)
                     return HttpNotFound();
+
                 var ProductList = new MainCategoryViewModel()
                 {
                     Category = categorie,
                     Products = _context.Product.Where(c => c.Name.Contains(searchString)).ToList().AsEnumerable()
                     
                 };
+
                 if ( ProductList.Products.ToList().Count == 0)
                 {
                     ViewBag.Message = "Geen overeenkomende zoekresultaten op: " + searchString;
-            }
+                }
 
             return View(ProductList);
-                   }
+        }
 
     }
 }
--- DHDomtica/Controllers/StoreController.cs
+++ DHDomtica/Controllers/StoreController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DHDomtica.Models;
@@ -12,9 +14,61 @@ namespace DHDomtica.Controllers
         private DHDomoticaDBEntities db = new DHDomoticaDBEntities();
 
         // GET: Store
-        public ViewResult Index()
+        public ActionResult Index()
         {
-            return View(db.Product.ToList());
+            
+            return View();
+        }
+
+        //Code for the Store/ProductDetails
+        // GET: ProductDetails
+        public ActionResult ProductDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ShowSidebar();
+            return View(product);
+        }
+
+        //Todo: Make this work
+        public ActionResult Wishlist()
+        {
+            ViewBag.Message = "Yes, We are all wishing for a wishlist...";
+            return View();
+        }
+
+        //Copied code from HomeController for the _sidebar
+        private void ShowSidebar()
+        {
+            System.Diagnostics.Debug.WriteLine($"Sidebar {Request.RawUrl}");
+            ViewBag.ShowSideBar = true;
+            ViewBag.AllCategories = db.MainCategory.ToList();
+            ViewBag.AllProducts = db.Product.ToList();
+        }
+
+        //Generating ProductList by category ID
+        public ActionResult Categories(int id)
+        {
+            ShowSidebar();
+            var categorie = db.MainCategory.SingleOrDefault(c => c.ID == id);
+
+            if (categorie == null)
+                return HttpNotFound();
+
+            var ProductList = new MainCategoryViewModel()
+            {
+                Category = categorie,
+                Products = db.Product.Where(c => c.MainCategoryID.Equals(id)).ToList().AsEnumerable()
+
+            };
+            return View(ProductList);
         }
     }
 }
