e02332459fda2f567dd13d02f1755fd3c2f0aa71
LorroWijn <33690232+LorroWijn@users.noreply.github.com>
2018-10-28 15:19:21 +0100

    Begin Signin

--- DHDomtica/Controllers/SignInController.cs
+++ DHDomtica/Controllers/SignInController.cs
@@ -1,45 +1,90 @@
-﻿using System.Linq;
+﻿//using System.Linq;
+//using System.Web.Mvc;
+//using DHDomtica.Models;
+
+//namespace MvcLoginAppDemo.Controllers
+//{
+//    public class SignInController : Controller
+//    {
+//        public ActionResult InlogPage()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult InlogPage(User userModel)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                using (DHDomoticaDBEntities db = new DHDomoticaDBEntities())
+//                {
+//                    var obj = db.User.Where(a => a.EMail.Equals(userModel.EMail) && a.Password.Equals(userModel.Password)).FirstOrDefault();
+//                    if (obj != null)
+//                    {
+//                        Session["UserID"] = obj.ID.ToString();
+//                        Session["UserEmail"] = obj.EMail.ToString();
+//                        return RedirectToAction("UserDashBoard");
+//                    }
+//                }
+//            }
+//            return View(userModel);
+//        }
+
+//        public ActionResult UserDashBoard()
+//        {
+//            if (Session["UserID"] != null)
+//            {
+//                return View();
+//            }
+//            else
+//            {
+//                return RedirectToAction("InlogPage");
+//            }
+//        }
+//    }
+//}
+
+//Sign in 2.0
+using System.Linq;
 using System.Web.Mvc;
 using DHDomtica.Models;
+using DHDomtica.Supportclasses;
 
-namespace MvcLoginAppDemo.Controllers
+namespace DHDomtica.Controllers
 {
     public class SignInController : Controller
     {
-        public ActionResult InlogPage()
+
+        [HttpGet]
+        // GET: SignUp
+        public ActionResult InlogPage(int id = 0)
         {
-            return View();
+            User usermodel = new User();
+            return View(usermodel);
         }
 
-        [HttpPost]
+        [HttpGet]
         [ValidateAntiForgeryToken]
-        public ActionResult InlogPage(User userModel)
+        public ActionResult Inloggen(User userModel)
         {
-            if (ModelState.IsValid)
+            using (DHDomoticaDBEntities DHDomoticadbModel = new DHDomoticaDBEntities())
             {
-                using (DHDomoticaDBEntities db = new DHDomoticaDBEntities())
-                {
-                    var obj = db.User.Where(a => a.EMail.Equals(userModel.EMail) && a.Password.Equals(userModel.Password)).FirstOrDefault();
-                    if (obj != null)
-                    {
-                        Session["UserID"] = obj.ID.ToString();
-                        Session["UserEmail"] = obj.EMail.ToString();
-                        return RedirectToAction("UserDashBoard");
-                    }
-                }
-            }
-            return View(userModel);
-        }
+                var ePwd = Crypto.Hash(userModel.Password);
+                var x = DHDomoticadbModel.User.FirstOrDefault(u => u.EMail == userModel.EMail && u.Password == ePwd);
 
-        public ActionResult UserDashBoard()
-        {
-            if (Session["UserID"] != null)
-            {
-                return View();
-            }
-            else
-            {
-                return RedirectToAction("InlogPage");
+                if (x == null)
+                {
+                    //return View("Store/Index", new User());
+                    return View("Index", new User());
+                    // "Yo werkt niet maat";
+                }
+                else
+                {
+                    //je bent ingelogd
+                    //return View("Home/InlogPage", new User());
+                    return View("Index", new User());
+                }
             }
         }
     }
