efc73ac827127387126f087b77e4a2c46ea1d8b7
DZoetmulder <dannyzoetmulder@gmail.com>
2018-11-21 12:25:55 +0100

    Models weer gefixt

--- DHDomtica/Models/User.cs
+++ DHDomtica/Models/User.cs
@@ -11,20 +11,66 @@ namespace DHDomtica.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
     public partial class User
     {
         public int ID { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Voornaam")]
+        [Required(ErrorMessage = "Vul Uw voornaam in")]
         public string FirstName { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Achternaam")]
+        [Required(ErrorMessage = "Vul Uw achternaam in")]
         public string LastName { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Geslacht")]
+        [Required(ErrorMessage = "Kies Uw geslacht")]
         public string Gender { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "Vul Uw E-mailadres in")]
+        [RegularExpression(@"^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$", ErrorMessage = "Vul een correct E-mailadres in, zoals 0956570@hr.nl")]
         public string EMail { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Wachtwoord")]
+        [Required(ErrorMessage = "Vul Uw wachtwoord in")]
         public string Password { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Land")]
+        [Required(ErrorMessage = "Kies Uw land")]
         public string Country { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Provincie")]
+        [Required(ErrorMessage = "Kies Uw provincie")]
         public string Province { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Stad")]
+        [Required(ErrorMessage = "Vul Uw stad in")]
         public string City { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [DisplayName("Postcode")]
+        [Required(ErrorMessage = "Vul Uw postcode in")]
+        [RegularExpression(@"^[1-9][0-9]{3}\s*(?:[a-zA-Z]{2})?$", ErrorMessage = "Vul een correcte Nederlandse postcode in, zoals 3011 WN")]
         public string ZipCode { get; set; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Woonadres")]
+        [Required(ErrorMessage = "Vul Uw woonadres in")]
+        [RegularExpression(@"^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$", ErrorMessage = "Vul een correct Nederlands adres in zonder komma, zoals Wijnhaven 107")]
         public string BillingAddress { get; set; }
+
         public Nullable<int> AdminID { get; set; }
     }
 }
