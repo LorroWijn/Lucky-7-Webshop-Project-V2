faa0336c108b994a6acd01cdf31e005208e9db82
DZoetmulder <dannyzoetmulder@gmail.com>
2018-11-25 20:13:19 +0100

    Een zooi Uw s verwijderd.

--- DHDomtica/Models/User.cs
+++ DHDomtica/Models/User.cs
@@ -28,54 +28,54 @@ namespace DHDomtica.Models
 
         [DataType(DataType.Text)]
         [DisplayName("Voornaam")]
-        [Required(ErrorMessage = "Vul Uw voornaam in")]
+        [Required(ErrorMessage = "Vul uw voornaam in")]
         public string FirstName { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Achternaam")]
-        [Required(ErrorMessage = "Vul Uw achternaam in")]
+        [Required(ErrorMessage = "Vul uw achternaam in")]
         public string LastName { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Geslacht")]
-        [Required(ErrorMessage = "Kies Uw geslacht")]
+        [Required(ErrorMessage = "Kies uw geslacht")]
         public string Gender { get; set; }
 
         [DataType(DataType.EmailAddress)]
         [DisplayName("E-mail")]
-        [Required(ErrorMessage = "Vul Uw E-mailadres in")]
+        [Required(ErrorMessage = "Vul uw E-mailadres in")]
         [RegularExpression(@"^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{2,7}$", ErrorMessage = "Vul een correct E-mailadres in, zoals 0956570@hr.nl")]
         public string EMail { get; set; }
 
         [DataType(DataType.Password)]
         [DisplayName("Wachtwoord")]
-        [Required(ErrorMessage = "Vul Uw wachtwoord in")]
+        [Required(ErrorMessage = "Vul uw wachtwoord in")]
         public string Password { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Land")]
-        [Required(ErrorMessage = "Kies Uw land")]
+        [Required(ErrorMessage = "Kies uw land")]
         public string Country { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Provincie")]
-        [Required(ErrorMessage = "Kies Uw provincie")]
+        [Required(ErrorMessage = "Kies uw provincie")]
         public string Province { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Stad")]
-        [Required(ErrorMessage = "Vul Uw stad in")]
+        [Required(ErrorMessage = "Vul uw stad in")]
         public string City { get; set; }
 
         [DataType(DataType.PostalCode)]
         [DisplayName("Postcode")]
-        [Required(ErrorMessage = "Vul Uw postcode in")]
+        [Required(ErrorMessage = "Vul uw postcode in")]
         [RegularExpression(@"^[1-9][0-9]{3}\s*(?:[a-zA-Z]{2})?$", ErrorMessage = "Vul een correcte Nederlandse postcode in, zoals 3011 WN")]
         public string ZipCode { get; set; }
 
         [DataType(DataType.Text)]
         [DisplayName("Woonadres")]
-        [Required(ErrorMessage = "Vul Uw woonadres in")]
+        [Required(ErrorMessage = "Vul uw woonadres in")]
         [RegularExpression(@"^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$", ErrorMessage = "Vul een correct Nederlands adres in zonder komma, zoals Wijnhaven 107")]
         public string BillingAddress { get; set; }
 
--- DHDomtica/ViewModels/UserPasswordChangeViewModel.cs
+++ DHDomtica/ViewModels/UserPasswordChangeViewModel.cs
@@ -11,17 +11,17 @@ namespace DHDomtica.Models
     {
         [DataType(DataType.Password)]
         [DisplayName("Huidig wachtwoord")]
-        [Required(ErrorMessage = "Vul Uw oude wachtwoord in")]
+        [Required(ErrorMessage = "Vul uw oude wachtwoord in")]
         public string OldPassword { get; set; }
 
         [DataType(DataType.Password)]
         [DisplayName("Nieuw wachtwoord")]
-        [Required(ErrorMessage = "Vul Uw nieuwe wachtwoord in")]
+        [Required(ErrorMessage = "Vul uw nieuwe wachtwoord in")]
         public string ChangePassword { get; set; }
 
         [DataType(DataType.Password)]
         [DisplayName("Verificatie nieuw wachtwoord")]
-        [Required(ErrorMessage = "Bevestig Uw nieuwe wachtwoord")]
+        [Required(ErrorMessage = "Bevestig uw nieuwe wachtwoord")]
         [Compare("ChangePassword")]
         public string ChangeConfirmPassword { get; set; }
     }
