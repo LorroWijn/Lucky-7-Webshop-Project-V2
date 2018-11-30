c16cb7e920d3838cab099ed6862fe972e56c70f3
sfbuvelot <s.f.buvelot@gmail.com>
2018-11-30 13:33:20 +0100

    omdat het kan

--- DHDomtica/Models/User.cs
+++ DHDomtica/Models/User.cs
@@ -14,33 +14,15 @@ namespace DHDomtica.Models
     
     public partial class User
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public User()
-        {
-            this.Wishlists = new HashSet<Wishlist>();
-            this.Reviews = new HashSet<Review>();
-            this.Orders = new HashSet<Order>();
-        }
-    
         public int ID { get; set; }
-        public int AdminID { get; set; }
+        public string NickName { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
-        public string EMail { get; set; }
-        public string Password { get; set; }
-        public string Country { get; set; }
-        public string Province { get; set; }
-        public string City { get; set; }
         public string BillingAddress { get; set; }
         public string ZipCode { get; set; }
-    
-        public virtual AdminRight AdminRight { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Wishlist> Wishlists { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Review> Reviews { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Order> Orders { get; set; }
+        public string City { get; set; }
+        public string Land { get; set; }
     }
 }
